0263643c0e4944cffbd85c0a542e4d9794af8826
alvaroof <ortiz.fernandez.alvaro@gmail.com>
2021-05-29 11:44:07 +0200

    Trying bootstrap 3

--- pelicanconf.py
+++ pelicanconf.py
@@ -2,25 +2,26 @@ from datetime import datetime
 
 AUTHOR = 'Alvaro Ortiz'
 SITENAME = 'Data-Gizmo'
-SITEURL = 'http://username.github.io'
+SITEURL = 'http://alvaroof.github.io'
 SITETITLE = "Data-Gizmo"
-SITESUBTITLE = "The minimalist Pelican theme"
-SITEDESCRIPTION = "Flex - The minimalist Pelican theme."
+# SITESUBTITLE = "The minimalist Pelican theme"
+# SITEDESCRIPTION = "Flex - The minimalist Pelican theme."
 # SITELOGO = ''
 # FAVICON = '/images/favicon.ico'
 BROWSER_COLOR = "#333333"
 PYGMENTS_STYLE = "monokai"
 
-ROBOTS = "index, follow"
+# ROBOTS = "index, follow"
 
 #THEME = "Flex"
-THEME = "pelican-blue"
+#THEME = "pelican-blue"
+THEME = "pelican-bootstrap3"
 
 PATH = "content"
 OUTPUT_PATH = "output"
 TIMEZONE = "Europe/Madrid"
 
-DISABLE_URL_HASH = True
+# DISABLE_URL_HASH = True
 
 MARKUP = ("md", "ipynb")
 
@@ -46,17 +47,17 @@ DATE_FORMATS = {
 
 FEED_ALL_ATOM = "feeds/all.atom.xml"
 CATEGORY_FEED_ATOM = "feeds/{slug}.atom.xml"
-TRANSLATION_FEED_ATOM = None
-AUTHOR_FEED_ATOM = None
-AUTHOR_FEED_RSS = None
+#TRANSLATION_FEED_ATOM = None
+#AUTHOR_FEED_ATOM = None
+#AUTHOR_FEED_RSS = None
 
 USE_FOLDER_AS_CATEGORY = False
-MAIN_MENU = True
-HOME_HIDE_TAGS = True
+#MAIN_MENU = True
+#HOME_HIDE_TAGS = True
 
 SOCIAL = (
-    ("github", "https://github.com/alexandrevicenzi/Flex"),
-    ("rss", "/blog/feeds/all.atom.xml"),
+    ("github", "https://github.com/alvaroof"),
+    ("linkedin", "https://linkedin.com/in/alvaroof"),
 )
 
 MENUITEMS = (
@@ -64,14 +65,6 @@ MENUITEMS = (
     ("Categories", "/categories.html"),
     ("Tags", "/tags.html"),
 )
-
-CC_LICENSE = {
-    "name": "Creative Commons Attribution-ShareAlike 4.0 International License",
-    "version": "4.0",
-    "slug": "by-sa",
-    "icon": True,
-    "language": "en_US",
-}
 
 COPYRIGHT_YEAR = datetime.now().year
 DEFAULT_PAGINATION = 10
@@ -86,16 +79,14 @@ DEFAULT_PAGINATION = 10
 #    "extra/CNAME": {"path": "CNAME"},
 #}
 
-THEME_COLOR_AUTO_DETECT_BROWSER_PREFERENCE = True
-THEME_COLOR_ENABLE_USER_OVERRIDE = True
+# THEME_COLOR_AUTO_DETECT_BROWSER_PREFERENCE = True
+# THEME_COLOR_ENABLE_USER_OVERRIDE = True
 
-USE_LESS = True
-
-
+# USE_LESS = True
 
 # pelican-blue
-SIDEBAR_DIGEST = 'Programmer and Web Developer'
-FAVICON = 'url-to-favicon'
-DISPLAY_PAGES_ON_MENU = True
-TWITTER_USERNAME = 'twitter-user-name'
-MENUITEMS = (('Blog', SITEURL),)
+# SIDEBAR_DIGEST = 'Programmer and Web Developer'
+# FAVICON = 'url-to-favicon'
+# DISPLAY_PAGES_ON_MENU = True
+# TWITTER_USERNAME = 'twitter-user-name'
+# MENUITEMS = (('Blog', SITEURL),)
